793b94c1ea9dcbe116e2ffb26abd719ddadbc414
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-07-16 19:24:27 -0700

    c

---
+++ week-8/gonzalez-discussion-8.1.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  ("button").click(function(){ <!-- One Error -->
+    $("fade1").fadeIn("slow"); <!-- One Error -->
+  });
+});
+</script>
+</head>
+<body>
+<button>Click For Fade In</button><br><br>
+
+<div id ="fade1" style="display:none;background-color:red;width:36px;"> Hello </div>
+
+<script>
+
+</script>
+
+</body>
+</html>
